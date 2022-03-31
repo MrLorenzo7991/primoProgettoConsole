c57991c38ec11e2bbee436fb9715e5ac17304f84
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-31 11:51:35 +0200

    Aggiungere i file di progetto.

---
+++ primoProgettoConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); //stampa in console e va a capo
+
+Console.Write("Aggiungi un nome: ");  //stesssa cosa ma dopo non va a capo (scrive sulla stessa linea)
+
+var testo = Console.ReadLine();  //Legge un input da console e lo inserisce in una variabile "testo"
+
+Console.WriteLine("Il tuo nome è " + testo);
